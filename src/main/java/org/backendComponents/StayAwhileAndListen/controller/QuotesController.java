f8daeddf891cd49a10f25a81f2fceb8007a98755
Kamil <kastarland@gmail.com>
2021-02-07 02:11:08 +0100

    aaa

--- src/main/java/org/backendComponents/StayAwhileAndListen/controller/QuotesController.java
+++ src/main/java/org/backendComponents/StayAwhileAndListen/controller/QuotesController.java
@@ -1,12 +1,14 @@
 package org.backendComponents.StayAwhileAndListen.controller;
 
 import org.backendComponents.StayAwhileAndListen.model.Diablo2Character;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/stayAwhile")
+@CrossOrigin(origins = "http://localhost:3000")
 public class QuotesController {
 
     @GetMapping("/test")
--- src/main/webapp/stay_awhile_and_listenjs/src/App.js
+++ src/main/webapp/stay_awhile_and_listenjs/src/App.js
@@ -1,28 +1,44 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+    state = {
+        isLoading: true,
+        Diablo2Characters: []
+    };
 
+    async componentDidMount() {
+        const options = {
+            headers: {
+                method: "GET"
+            }
+        }
+        const response = await fetch('http://localhost:8080/stayAwhile/test', options);
+        const body = await response.json();
+        this.setState({Diablo2Character: body, isLoading: false});
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LOLOLO
-        </a>
-
-      </header>
-    </div>
-  );
+    render() {
+        const {Diablo2Character, isLoading} = this.state;
+        if (isLoading) {
+            return <p>Loading...</p>;
+        }
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <div className="App-intro">
+                            <h2>JESTĘ PROGRAMISTĄ</h2>
+                            <div>
+                                {Diablo2Character.description}
+                                {Diablo2Character.name}
+                            </div>
+                        </div>
+                    </header>
+                </div>
+            );
+        return <div>madzia to swietna przyjaciolka</div>
+    }
 }
-
 export default App;
